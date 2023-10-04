dd482faa659a246ceb3311de9ca0a051808e2f16
KeziaSantoso <keziaxia@gmail.com>
2023-10-04 23:46:52 +0700

    Add LVinnamon

---
+++ product.js
@@ -0,0 +1,35 @@
+var productContainer = document.getElementById('product-container');
+  
+    // Iterate over products array and create product elements
+    products.forEach(function(product) {
+      var productElement = document.createElement('div');
+      productElement.className = 'product';
+  
+      var imageElement = document.createElement('img');
+      imageElement.className = 'product-image';
+      imageElement.src = product.productImage;
+      imageElement.alt = product.productName;
+      productElement.appendChild(imageElement);
+  
+      var detailsElement = document.createElement('div');
+      detailsElement.className = 'product-details';
+  
+      var nameElement = document.createElement('div');
+      nameElement.className = 'product-name';
+      nameElement.textContent = product.productName;
+      detailsElement.appendChild(nameElement);
+  
+      var priceElement = document.createElement('div');
+      priceElement.className = 'product-price';
+      priceElement.textContent = 'Price: Rp. ' + product.productPrice ;
+      detailsElement.appendChild(priceElement);
+  
+      var descriptionElement = document.createElement('div');
+      descriptionElement.className = 'product-description';
+      descriptionElement.textContent = product.productDescription;
+      detailsElement.appendChild(descriptionElement);
+  
+      productElement.appendChild(detailsElement);
+  
+      productContainer.appendChild(productElement);
+    });
